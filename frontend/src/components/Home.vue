0f1b72f5fb90faae64e1b14760c2ccdf2d5db90e
burak <rburaksaritas@gmail.com>
2023-11-09 20:30:11 +0300

    Add create button & form

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -16,6 +16,16 @@
           </div>
         </router-link>
       </div>
+      <button @click="showCreateForm = true" class="create-button">+ Add New Product</button>
+      <div v-if="showCreateForm" class="create-form">
+        <input v-model="createForm.name" placeholder="Name">
+        <textarea v-model="createForm.description" placeholder="Description"></textarea>
+        <input v-model="createForm.price" placeholder="Price">
+        <input v-model="createForm.image_url" placeholder="Image URL">
+        <button @click="createProduct">Create Product</button>
+        <button @click="showCreateForm = false">Cancel</button>
+      </div>
+
     </div>
     <footer class="footer">
       <p>2023 &copy; <a href="https://github.com/rburaksaritas"><strong>github.com/rburaksaritas</strong></a></p>
@@ -30,7 +40,14 @@ export default {
   name: 'home-page',
   data() {
     return {
-      products: []
+      products: [],
+      showCreateForm: false, // To toggle the create form
+      createForm: { // To bind form inputs for a new product
+        name: '',
+        description: '',
+        price: 0,
+        image_url: ''
+      }
     };
   },
   mounted() {
@@ -45,7 +62,25 @@ export default {
         console.error(error);
         // Handle error here, e.g., user feedback or a retry mechanism.
       }
-    }
+    },
+    async createProduct() {
+      try {
+        const response = await axios.post('/api/products', this.createForm);
+        this.products.push(response.data); // Add the new product to the list
+        this.showCreateForm = false; // Hide the form after creation
+        // Reset the form
+        this.createForm = {
+          name: '',
+          description: '',
+          price: 0,
+          image_url: ''
+        };
+        // Optionally, display a success message or handle redirection
+      } catch (error) {
+        console.error('Error creating product:', error);
+        // Handle error here, such as displaying an error message to the user
+      }
+    },
   }
 };
 </script>
@@ -133,17 +168,61 @@ body {
 
 /* Add responsive design */
 @media (min-width: 768px) {
-    .product-detail-card {
-        flex-direction: row;
-    }
+  .product-detail-card {
+    flex-direction: row;
+  }
 
-    .product-detail-info {
-        margin-left: 2rem;
-    }
+  .product-detail-info {
+    margin-left: 2rem;
+  }
 
-    .product-detail-image {
-        max-width: 50%;
-    }
+  .product-detail-image {
+    max-width: 50%;
+  }
+}
+.create-form {
+    display: flex;
+    flex-direction: column;
+    max-width: 800px;
+    margin: 1rem auto;
+}
+
+.create-form input,
+.create-form textarea {
+    margin-bottom: 0.5rem;
+    padding: 0.5rem;
+    border: 1px solid #ddd;
+    border-radius: 4px;
+}
+
+.create-form button {
+    padding: 0.5rem 1rem;
+    margin-top: 0.5rem;
+    border: none;
+    border-radius: 4px;
+    background-color: #5cb85c;
+    color: white;
+    cursor: pointer;
+}
+
+.create-form button:hover {
+    background-color: #4cae4c;
+}
+
+.create-button {
+  padding: 0.5rem 1rem;
+  margin: 1rem;
+  border: none;
+  border-radius: 4px;
+  background-color: #28a745; /* Bootstrap success color for create button */
+  color: white;
+  font-weight: bold;
+  cursor: pointer;
+  outline: none; /* Remove outline */
+}
+
+.create-button:hover {
+  background-color: #218838; /* Darken color on hover */
 }
 
 .footer {
@@ -168,5 +247,6 @@ body {
   /* Lighter shade of white for hover effect */
   text-decoration: none;
   /* Optional: adds underline on hover */
-}</style>
+}
+</style>
 
