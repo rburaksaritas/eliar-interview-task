682539f64dba968644c4d9e56cbfa6ecd933ac3c
burak <rburaksaritas@gmail.com>
2023-11-10 15:04:43 +0300

    Update inital data

--- database/2_createInitialProducts.sql
+++ database/2_createInitialProducts.sql
@@ -1,12 +1,12 @@
 -- Insert initial data into the 'products' table
 
 INSERT INTO products (name, description, price, image_url) VALUES
-('RD96', 'Tam Otomatik Sıvı Kimyasal Otomasyon Sistemi', 5000.00, 'url_to_image_rd96.jpg'),
-('ADW', 'Tam Otomatik Toz Boyarmadde Otomasyon Sistemi', 4500.00, 'url_to_image_adw.jpg'),
-('RDD', 'Tam Otomatik Toz Boyarmadde Otomasyon Sistemi', 4700.00, 'url_to_image_rdd.jpg'),
-('SLD BASIC', 'Tam Otomatik Sıvı Kimyasal Otomasyon Sistemi', 3000.00, 'url_to_image_sld_basic.jpg'),
-('CDD', 'Toz Boyarmadde Otomasyon Sistemi', 3500.00, 'url_to_image_cdd.jpg'),
-('SLD HYPO', 'Tam Otomatik Sıvı Kimyasal Otomasyon Sistemi', 5500.00, 'url_to_image_sld_hypo.jpg'),
-('LABx', 'Tam Otomatik Laboratuvar Dozajlama Otomasyon Sistemi', 6000.00, 'url_to_image_labx.jpg'),
-('PATERNOSTER', 'Toz Boyarmadde Tartım & Depolama Sistemi', 8000.00, 'url_to_image_paternoster.jpg'),
-('PPD', 'Tam Otomatik Baskı Boya Mutfağı', 7500.00, 'url_to_image_ppd.jpg');
+('RD96', 'Tam Otomatik Sıvı Kimyasal Otomasyon Sistemi', 5000.00, 'https://www.eliar.com/wp-content/uploads/2021/10/rd96-web-uai-258x194.jpg'),
+('ADW', 'Tam Otomatik Toz Boyarmadde Otomasyon Sistemi', 4500.00, 'https://www.eliar.com/wp-content/uploads/2022/08/ADW_WEB3-uai-258x194.jpg'),
+('RDD', 'Tam Otomatik Toz Boyarmadde Otomasyon Sistemi', 4700.00, 'https://www.eliar.com/wp-content/uploads/2022/08/RDD-web-uai-258x194.jpg'),
+('SLD BASIC', 'Tam Otomatik Sıvı Kimyasal Otomasyon Sistemi', 3000.00, 'https://www.eliar.com/wp-content/uploads/2021/03/sld_basic-uai-258x194.jpg'),
+('CDD', 'Toz Boyarmadde Otomasyon Sistemi', 3500.00, 'https://www.eliar.com/wp-content/uploads/2022/08/CDD_web-uai-258x194.jpg'),
+('SLD HYPO', 'Tam Otomatik Sıvı Kimyasal Otomasyon Sistemi', 5500.00, 'https://www.eliar.com/wp-content/uploads/2021/03/SLD_Hypo-uai-258x194.jpg'),
+('LABx', 'Tam Otomatik Laboratuvar Dozajlama Otomasyon Sistemi', 6000.00, 'https://www.eliar.com/wp-content/uploads/2022/12/LABX-ELIAR-uai-258x194.jpg'),
+('PATERNOSTER', 'Toz Boyarmadde Tartım & Depolama Sistemi', 8000.00, 'https://www.eliar.com/wp-content/uploads/2021/03/paternoster_web-uai-258x194.jpg'),
+('PPD', 'Tam Otomatik Baskı Boya Mutfağı', 7500.00, 'https://www.eliar.com/wp-content/uploads/2022/08/pat-uai-258x194.jpg');
